b3e25e2e6a44bdc58f91ab4e8dda0dabf2d7a323
Rudi MK <rudimk@live.com>
2012-07-17 08:12:15 -0700

    Initial commit. Pulling name and profile pic from Graph API.

---
+++ FBMetro III/FbInfo.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Facebook;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FBMetro_III
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FbInfo : Page
+    {
+
+        private readonly FacebookClient _fb = new FacebookClient();
+        private string _userId;
+
+        public FbInfo()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            dynamic parameter = e.Parameter;
+            _fb.AccessToken = parameter.access_token;
+            _userId = parameter.id;
+
+            GetUserProfilePicture();
+            GetUserName();
+        }
+
+        private async void GetUserProfilePicture()
+        {
+            try
+            {
+                dynamic result = await _fb.GetTaskAsync("me?fields=picture");
+                string id = result.id;
+
+                // available picture types: square (50x50), small (50xvariable height), large (about 200x variable height) (all size in pixels)
+                // for more info visit http://developers.facebook.com/docs/reference/api
+                string profilePictureUrl = string.Format("https://graph.facebook.com/{0}/picture?type={1}&access_token={2}", _userId, "square", _fb.AccessToken);
+
+                imgProfilePic.Source = new BitmapImage(new Uri(profilePictureUrl));
+            }
+            catch (FacebookApiException ex)
+            {
+                // Write an error handler of some sort.
+            }
+        }
+
+        private async void GetUserName()
+        {
+            try
+            {
+                dynamic result2 = await _fb.GetTaskAsync("me?fields=name");
+                txbUserName.Text = result2.name;
+            }
+            catch (FacebookApiException ex)
+            {
+                // Again, write some sort of error handler.
+            }
+        }
+    }
+}
---
+++ FBMetro III/FbLogin.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Facebook;
+using System.Dynamic;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FBMetro_III
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FbLogin : Page
+    {
+       
+        //Define a Facebook App ID, as well as extended permissions.
+        private const string AppId = "220287254759782";
+        private const string ExtendedPermissions = "user_about_me,read_stream,publish_stream";
+        
+        // Initialize the Facebook Client.
+        private readonly FacebookClient _fb = new FacebookClient();
+ 
+        public FbLogin()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void WebView_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            WebView1.LoadCompleted += WebView1_LoadCompleted;
+
+            var loginUrl = GetFacebookLoginUrl(AppId, ExtendedPermissions);
+            WebView1.Navigate(loginUrl);
+        }
+
+        private Uri GetFacebookLoginUrl(string appId, string extendedPermissions)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.client_id = appId;
+            parameters.redirect_uri = "https://www.facebook.com/connect/login_success.html";
+            parameters.response_type = "token";
+            parameters.display = "popup";
+
+            // add the 'scope' parameter only if we have extendedPermissions.
+            if (!string.IsNullOrWhiteSpace(extendedPermissions))
+            {
+                // A comma-delimited list of permissions
+                parameters.scope = extendedPermissions;
+            }
+
+            return _fb.GetLoginUrl(parameters);
+        }
+
+        private void WebView1_LoadCompleted(object sender, Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            FacebookOAuthResult oauthResult;
+            if (!_fb.TryParseOAuthCallbackUrl(e.Uri, out oauthResult))
+            {
+                return;
+            }
+
+            if (oauthResult.IsSuccess)
+            {
+                var accessToken = oauthResult.AccessToken;
+                LoginSucceded(accessToken);
+            }
+            else
+            {
+                // user cancelled
+            }
+        }
+
+        private async void LoginSucceded(string accessToken)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.access_token = accessToken;
+            parameters.fields = "id";
+
+            dynamic result = await _fb.GetTaskAsync("me", parameters);
+            parameters = new ExpandoObject();
+            parameters.id = result.id;
+            parameters.access_token = accessToken;
+
+            Frame.Navigate(typeof(FbInfo), (object)parameters);
+        }
+    }
+}
